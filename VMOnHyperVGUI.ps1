49c51e2ce4c094cbb12364b355282c32fb062250
Tomas Johansson <tomas@4thcorner.net>
2020-06-22 14:25:40 +0200

    Update VMOnHyperVGUI.ps1

--- VMOnHyperVGUI.ps1
+++ VMOnHyperVGUI.ps1
@@ -1,11 +1,12 @@
 ﻿<#
  Solution: Hyper-V 
  Purpose: Hyper-V GUI
- Version: 1.0
-    Date: 26 May 2016
+ Version: 2.0.0
+    Date: 22 June 2020 
+
   Author: Tomas Johansson
- Twitter: @CanKlant
-    Blog: http://www.4thcorner.se
+ Twitter: @Deploymentdude
+     WWW: www.4thcorner.net
 
  This script is provided "AS IS" with no warranties, confers no rights and 
  is not supported by the author
